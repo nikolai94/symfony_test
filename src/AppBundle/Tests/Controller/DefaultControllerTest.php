216bd31079eed31d5f88e18c40bdec7dbe17360b
Nikolai Hansen <nikolai@cybermanager.dk>
2017-08-30 16:31:14 +0200

    testing

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,9 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
+        var_dump($client->getResponse()->getContent());
+        var_dump($client->getRequest()->getUri());
+        
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
